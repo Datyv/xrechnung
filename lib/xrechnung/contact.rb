84fa1d002a88e0e1c6bb231d83ade636162da0ea
datyv <goizet@taxi.de>
2025-02-06 14:20:03 +0100

    contact telephone optional

--- lib/xrechnung/contact.rb
+++ lib/xrechnung/contact.rb
@@ -8,18 +8,18 @@ module Xrechnung
 
     # @!attribute telephone
     #   @return [String]
-    member :telephone, type: String
+    member :telephone, type: String, optional: true
 
     # @!attribute electronic_mail
     #   @return [String]
-    member :electronic_mail, type: String
+    member :electronic_mail, type: String, optional: true
 
     # noinspection RubyResolve
     def to_xml(xml)
       xml.cac :Contact do
         xml.cbc :Name, name
-        xml.cbc :Telephone, telephone
-        xml.cbc :ElectronicMail, electronic_mail
+        xml.cbc :Telephone, telephone if telephone.present?
+        xml.cbc :ElectronicMail, electronic_mail if electronic_mail.present?
       end
     end
   end
